fed189f13ee03ed8d589282158afc9eefa7f9158
YUVARAJ SINGH <126282771+yuvakali@users.noreply.github.com>
2023-10-22 14:19:03 +0530

    Add files via upload

---
+++ random_forest_classifier.py
@@ -0,0 +1,98 @@
+# Random Forest Classifier
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the datasets
+
+datasets = pd.read_csv('Walmart.csv')
+X = datasets.iloc[:, [2,3]].values
+Y = datasets.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_Train = sc_X.fit_transform(X_Train)
+X_Test = sc_X.transform(X_Test)
+
+# Fitting the classifier into the Training set
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 200, criterion = 'entropy', random_state = 0)
+classifier.fit(X_Train,Y_Train)
+
+# Predicting the test set results
+
+Y_Pred = classifier.predict(X_Test)
+
+# Making the Confusion Matrix 
+
+from sklearn.metrics import confusion_matrix,f1_score
+cm = confusion_matrix(Y_Test, Y_Pred)
+f1 = f1_score(Y_Test, Y_Pred, average='weighted')
+
+# Visualising the Training set results
+
+from matplotlib.colors import ListedColormap
+X_Set, Y_Set = X_Train, Y_Train
+X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_Set)):
+    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+
+from matplotlib.colors import ListedColormap
+X_Set, Y_Set = X_Test, Y_Test
+X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_Set)):
+    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classifier on Sales Data(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Ordered')
+plt.legend()
+plt.show()
+
+# Display the Confusion Matrix
+import seaborn as sns
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
+
+# Display the F1 Score
+plt.figure(figsize=(6, 4))
+plt.bar(["F1 Score"], [f1], color="orange")
+plt.title("F1 Score")
+plt.ylim(0, 1)
+plt.show()
+
+
